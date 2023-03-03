e4f61ec5391b4b5c98f06587a718ceb5d776af2c
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-03-03 19:05:11 +0000

    Add prompt for asking question for team manager input

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const questions = [
+    {
+        type: 'input',
+        name: 'team_manager_name',
+        message: 'You will first fill out information about the team manager.\n What is their name?'
+    },
+    {
+        type: 'number',
+        name: 'team_manager_id',
+        message: 'employee id?'
+    },
+    {
+        type: 'input',
+        name: 'team_manager_email',
+        message: 'email address?'
+    },
+    {
+        type: 'number',
+        name: 'office_number',
+        message: 'office number?'
+    }
+]
+
+inquirer
+    .prompt(questions)
+    .then((answers) => {
+        console.log(JSON.stringify(answers, null, '  '));
+    })
